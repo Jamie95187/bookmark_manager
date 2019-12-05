c8f3b90b18b2ccc34d4209264501339494bde4e8
Jamie95187 <jamiewong63@googlemail.com>
2019-12-05 14:37:28 +0000

    refactor to work on webpage

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -5,7 +5,7 @@ class Bookmark
   def self.create(url:)
     database = ENV['ENVIRONMENT'] == 'test' ? 'bookmark_manager_test' : 'bookmark_manager'
     con = PG.connect(dbname: database, user: ENV['USER'])
-    con.exec("INSERT INTO bookmarks (url) VALUES (#{url});")
+    con.exec("INSERT INTO bookmarks (url) VALUES('#{url}');")
   end
 
   def self.all
